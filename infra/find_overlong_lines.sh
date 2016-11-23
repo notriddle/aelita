d060ca7fc6b7cf991696ee74b252cbc6aa6ac985
Michael Howell <michael@notriddle.com>
2016-11-23 14:09:02 -0700

    Lift the line limit to 120 characters

--- infra/find_overlong_lines.sh
+++ infra/find_overlong_lines.sh
@@ -1,3 +1,3 @@
 files=`find . | grep '\.\(rs\|md\|toml\|sh\|py$\)' | grep -v venv`
-grep '.\{80,\}' $files | grep -v '\(http\|https\)://'
+grep '.\{120,\}' $files | grep -v '\(http\|https\)://'
 if [ $? = 0 ]; then false; else true; fi
